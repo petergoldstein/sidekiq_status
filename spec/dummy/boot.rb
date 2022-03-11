455aeb24de8ba99eadc15ff67fc8f02715a5a902
Peter Goldstein <peter.m.goldstein@gmail.com>
2022-03-11 11:51:11 -0800

    Adjust class loading

--- spec/dummy/boot.rb
+++ spec/dummy/boot.rb
@@ -8,4 +8,4 @@ require 'active_support/dependencies'
 DUMMY_APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 Sidekiq.redis = {:url => "redis://localhost/15", :size => 5}
 
-ActiveSupport::Dependencies.autoload_paths += Dir.glob(DUMMY_APP_ROOT.join('app/*'))
+Dir[DUMMY_APP_ROOT.join('app/**/*.rb')].each { |f| require f }
